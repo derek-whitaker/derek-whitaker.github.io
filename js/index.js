75a0b2407701567adb4a07a0b0c3452cd313db70
Derek Whitaker <whitaker.derek@gmail.com>
2026-01-27 17:59:18 -0500

    Updated hero styling on case study pages

--- js/index.js
+++ js/index.js
@@ -34,3 +34,16 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     }
   });
 });
+
+// Header scroll effect for case study pages
+if (document.querySelector('.case-study-hero')) {
+  const header = document.querySelector('.header');
+
+  window.addEventListener('scroll', () => {
+    if (window.scrollY > 100) {
+      header.classList.add('scrolled');
+    } else {
+      header.classList.remove('scrolled');
+    }
+  });
+}
